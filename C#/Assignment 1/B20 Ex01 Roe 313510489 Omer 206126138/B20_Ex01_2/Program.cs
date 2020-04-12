e3ea26eb4c6da882ef575fae57c5e216add82822
itonroe <itonroe@gmail.com>
2020-04-12 17:56:43 +0300

    Init Seif 2, 3

---
+++ C#/Assignment 1/B20 Ex01 Roe 313510489 Omer 206126138/B20_Ex01_2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B20_Ex01_2
+{
+    class Program
+    {
+    }
+}
---
+++ C#/Assignment 1/B20 Ex01 Roe 313510489 Omer 206126138/B20_Ex01_3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B20_Ex01_3
+{
+    class Program
+    {
+    }
+}
