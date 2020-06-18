66b61aebf261e652f3766cf16756e1f8ab295b4f
Omer <omer.bentov@gmail.com>
2020-06-18 12:14:28 +0300

    .

---
+++ C#/assignment 4/Ex4/MainMenu.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    class MainManu
+    {
+        private string m_MenuName;
+        private List<MenuItem> m_MainMenu = new List<MenuItem>();
+
+        public MainManu()
+        {
+            //exit
+            MenuItem exit = new MenuItem();
+            exit.Text = "Exit";
+
+            m_MainMenu.Add(exit);
+            //create first top bar
+            MenuItem versionAndCapitals = new MenuItem();
+            versionAndCapitals.Text = "Version and Capitals";
+            versionAndCapitals.Parent = null;
+
+            MenuItem countCapitals = new MenuItem();
+            countCapitals.Text = "Count capitalss";
+            countCapitals.Parent = m_MainMenu;
+
+            MenuItem showVersion = new MenuItem();
+            showVersion.Text = "Show version";
+            showVersion.Parent = m_MainMenu;
+
+            versionAndCapitals.AddSubManuItem(countCapitals);
+            versionAndCapitals.AddSubManuItem(showVersion);
+
+            m_MainMenu.Add(versionAndCapitals);
+
+            //create second top bar
+            MenuItem showDateAndTime = new MenuItem();
+            showDateAndTime.Text = "Show date and time";
+
+            MenuItem showDate = new MenuItem();
+            showDate.Text = "Show date";
+            showVersion.Parent = m_MainMenu;
+
+            MenuItem showTime = new MenuItem();
+            showTime.Text = "Show time";
+            showVersion.Parent = m_MainMenu;
+
+            versionAndCapitals.AddSubManuItem(showDate);
+            versionAndCapitals.AddSubManuItem(showTime);
+
+            m_MainMenu.Add(showDateAndTime);
+
+        }
+
+        public void Show()
+        {
+            PrintMenu(m_MainMenu);
+        }
+
+        public List<MenuItem> PrintMenu(List<MenuItem> i_SubMenu, List<MenuItem> i_ParentManu)
+        {
+            PrintTitle("Title : ");
+            PrintSubMenu(subMenu);
+            int UserChoice = AskUser(subMenu.Count);
+            if (UserChoice == 0)
+                return subMenu[0].Parent;
+            else
+                return subMenu[UserChoice];
+        }
+
+        public void PrintTitle(string i_Title)
+        {
+            Console.WriteLine("Menu : {}", i_Title);
+        }
+
+        public void PrintSubMenu(List<MenuItem> i_MenuList)
+        {
+            foreach (MenuItem menuItem in i_MenuList)
+            {
+                Console.WriteLine(menuItem);
+            }
+        }
+
+        public int AskUser(int i_CountOfSubItem)
+        {
+            int goodChoice = 0;
+
+            Console.WriteLine("please choos...");
+            string choich = Console.ReadLine();
+            while(int.TryParse(choich,out goodChoice) && goodChoice >= 0 && goodChoice < i_CountOfSubItem)
+            {
+                Console.WriteLine("please choose...");
+                choich = Console.ReadLine();
+            }
+
+            return goodChoice;
+        }
+
+    }
+}
---
+++ C#/assignment 4/Ex4/MenuItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+
+    public class MenuItem
+    {
+        private string m_Text;
+        private List<MenuItem> m_Parent = null;
+        private List<MenuItem> m_SubManu = new List<MenuItem>();
+
+        public event Action<MenuItem> Click;
+
+        private void OnClicked()
+        {
+            if (Click != null)
+                Click.Invoke(this);
+        }
+        public string Text
+        {
+            get { return m_Text; }
+            set { m_Text = value; }
+        }
+
+        public List<MenuItem> Parent
+        {
+            get { return m_Parent; }
+            set { m_Parent = value; }
+        }
+
+        public override string ToString()
+        {
+            return "[{" + m_Text + "}]";
+        }
+
+        public void AddSubManuItem(MenuItem i_ManuItemToAdd)
+        {
+            m_SubManu.Add(i_ManuItemToAdd);
+        }
+
+    }
+}
