c45136580a9769cc7fbfc0b5d0eab5433b57e047
Omer <omer.bentov@gmail.com>
2020-08-25 17:48:55 +0300

    before parsing png ->xna

---
+++ Games/Ex1/Invaders/Classes/Ship.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Invaders.Classes
+{
+    class Ship
+    {
+        private GraphicsDeviceManager m_graphicsManager;
+        private SpriteBatch m_spriteBatch;
+        private GraphicsDevice m_graphicDevice;
+        private Microsoft.Xna.Framework.Content.ContentManager m_contentManager;
+
+        private readonly string m_texturePath = "Ship01_32x32";
+
+        Texture2D m_shipTexture;
+        Vector2 m_shipPosition;
+
+        Bullet m_bullet1;
+        Bullet m_bullet2;
+
+        public Ship(Vector2 i_shipPosition, GraphicsDeviceManager i_graphicsManager, SpriteBatch i_spriteBatch, GraphicsDevice i_graphicsDevice, Microsoft.Xna.Framework.Content.ContentManager i_contentManager)
+        {
+            m_graphicsManager = i_graphicsManager;
+            m_spriteBatch = i_spriteBatch;
+            m_shipPosition = i_shipPosition;
+            m_graphicDevice = i_graphicsDevice;
+            m_contentManager = i_contentManager;
+        }
+
+        protected void LoadContent()
+        {
+            m_shipTexture = m_contentManager.Load<Texture2D>(@"sprites\" + m_texturePath);
+            m_bullet1 = new Bullet(m_shipPosition, m_graphicsManager, m_spriteBatch, m_graphicDevice);
+            m_bullet2 = new Bullet(m_shipPosition, m_graphicsManager, m_spriteBatch, m_graphicDevice);
+        }
+
+        private void InitPosition()
+        {
+            float x = (float)m_graphicDevice.Viewport.Width / 2;
+            float y = (float)m_graphicDevice.Viewport.Height;
+        }
+
+        public void MoveRight(int i_distance)
+        {
+            if(m_shipPosition.X < (float)m_graphicDevice.Viewport.Width)
+                m_shipPosition.X += i_distance;
+        }
+
+        public void MoveLeft(int i_distance)
+        {
+            if(m_shipPosition.X > 0)
+                m_shipPosition.X -= i_distance;
+        }
+    }
+
+}
--- Games/Ex1/Invaders/Game1.cs
+++ Games/Ex1/Invaders/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Invaders.Classes;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,28 +7,37 @@ namespace Invaders
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager m_graphicsManager;
+        private SpriteBatch m_spriteBatch;
+
+        private Texture2D m_TextureBackground;
+
+        private Ship m_ship;
+
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            m_graphicsManager = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //init ship
+            float x = (float)GraphicsDevice.Viewport.Width / 2;
+            float y = (float)GraphicsDevice.Viewport.Height;
+            m_ship = new Ship(new Vector2(x, y), m_graphicsManager, m_spriteBatch, GraphicsDevice, Content);
 
             base.Initialize();
         }
 
+
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+            m_TextureBackground = Content.Load<Texture2D>("BG_Space01_1024x768");
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,7 +54,11 @@ namespace Invaders
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            m_spriteBatch.Begin();
+
+            m_spriteBatch.Draw(m_TextureBackground, new Vector2(0,0), Color.White); 
+
+            m_spriteBatch.End();
 
             base.Draw(gameTime);
         }
